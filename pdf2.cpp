ea9b94fe8619c2c2c60b9e55f35f7f9d91b06e29
cj2312 <49482388+cj2312@users.noreply.github.com>
2019-04-15 09:24:14 -0500

    Update pdf2.cpp

--- pdf2.cpp
+++ pdf2.cpp
@@ -3,7 +3,161 @@
 #include<ctime>
 
 using namespace std;
+//1.1.1
+void printNTimes(char *msg,int n)
+{
+	for (int i=0;i<n;i++)
+	{
+		cout<<msg<<endl;
+	}
+}
+//1.1.1.3
+void print(const long &x)
+{
+	cout<<x;
+}
+//1.1.3
+/***********
+int main()
+{
+	srand( time(5) );
 
+	int randNum=rand();
+	cout<<"A random number: "<<randNum<<endl;
+	return 0;
+}
+**********/
+//1.2.1
+void setString(char **strPtr)
+{
+    int x;
+    cin>>x;
+    if(x<0)
+        *strPtr = "Negatives!";
+    else
+        *strPtr = "Nonnegatives!";
+}
+//1.2.2
+/*********
+int *getRandNumPtr()
+{
+    int x = rand();
+    return &x;
+}
+int main()
+{
+	int *randNumPtr = getRandNumPtr();
+	cout << *randNumPtr; //error
+	return 0;
+}
+***********/
+//1.3.1
+/*************
+int main()
+{
+  const char *suitNames[] = {"Clubs ", "Diamonds ", "Spades ", "Clubs "};
+  cout << "Enter a suit number (1-4): ";
+  unsigned int suitNum;
+  cin >> suitNum;
+  if(suitNum <= 4)
+  cout << suitNames[suitNum -1];
+}
+************/
+//1.3.2
+/****************
+int main()
+{
+  const int ARRAY_LEN = 100;
+  int arr [ ARRAY_LEN ];
+  int *p = arr ;
+  int *q = & arr [0];
+}
+*****************/
+//problema 2
+/*********************
+void f(const int a=5)
+{
+	cout<<a*2<<"\n";
+}
+int a= 123;
+int main()
+{
+	f(1);
+	f(a);
+	int b=3;
+	f(b);
+	int a=4;
+	f(a);
+	f();
+
+
+}
+***********/
+
+//problema 3.1
+
+/************
+void printNum(int);//creo un prototipo antes de main
+//tambien podia ponerse la misma funcion antes del main
+int main()
+{
+	printNum(35);
+	return 0;
+}
+void printNum(int number){std::cout<<number;}
+***********/
+
+//problea 3.2
+
+/************
+void printNum(int number){std::cout<<number;} // declaro un argumento de tipo int
+// int number= 35 --- tambien podria glovalizar la varianle number
+int main()
+{
+	int number =35;
+	printNum(number);
+	return 0;
+}
+***************/
+
+//problema 3.3
+/*************
+
+void doubleNumber(int &num){num=num*2;}//paso por referencia para que asi el valor de num cambie
+int main()
+{
+	int num=35;
+	doubleNumber(num);
+	std::cout<<num;
+	return 0;
+}
+*************/
+//problema 3.4
+int difference( const int x, const int y) {
+int diff = abs(x -y);
+return diff;//retornamos diff
+// return abs(x-y); --- desechamos la diferencia y solo retornamos eel abs(x-y);
+
+}
+//problema 3.5
+
+int sum( const int x, const int y,const int z) {//agregamos el argumento z
+return x+y+z;
+}
+
+
+//problema 3.6
+
+/**************
+const int ARRAY_LEN = 10;
+int main() {
+	int arr[ARRAY_LEN]={10};
+
+	int *xPtr= arr, *yPtr= arr+ARRAY_LEN-1;//agregamos * antes del yPtr para que asi apunte a la ultima direccion del arr
+	std::cout << *xPtr<<" "<<*yPtr;
+return 0;
+}
+***********/
 //4.1
 int sum ( const int x , const int y ) {
     return x + y ;
@@ -13,9 +167,9 @@ double sum ( const double x , const double y ) {
     return x + y ;
 }
 //4.2
-//Mezclar y combinar un int con un doble hace que sea ambiguo a cuál quieres llamar.
-//El compilador podría convertir 1 a doble y llamar a la versión doble de sum,
-//o podría convertir 10.0 a un int y llamar a la versión int
+//Mezclar y combinar un int con un doble hace que sea ambiguo a cuÃ¡l quieres llamar.
+//El compilador podrÃ­a convertir 1 a doble y llamar a la versiÃ³n doble de sum,
+//o podrÃ­a convertir 10.0 a un int y llamar a la versiÃ³n int
 
 //4.3
 int sum ( const int x , const int y , const int z ) {
@@ -100,8 +254,8 @@ void transpose ( const int input [][ LENGTH ] , int output [][ WIDTH ]) {
     }
 }
 
-//6.4 Se devolvería un puntero al primer elemento de la matriz,
-//pero la matriz se habría ido Fuera de alcance, invalida el puntero.
+//6.4 Se devolverÃ­a un puntero al primer elemento de la matriz,
+//pero la matriz se habrÃ­a ido Fuera de alcance, invalida el puntero.
 
 //6.5
 void reverse (int numbers [] , const int numbersLen ) {
