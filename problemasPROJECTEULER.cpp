426c8ccfdfa5a4e7b2aa3acd862160a7a7d11928
cj2312 <49482388+cj2312@users.noreply.github.com>
2019-04-17 09:00:06 -0500

    Update problemasPROJECTEULER.cpp

--- problemasPROJECTEULER.cpp
+++ problemasPROJECTEULER.cpp
@@ -87,7 +87,30 @@ void print(int * arr,int t){
     }cout<<endl;
 }*/
 //problem 4----------------------
-
+bool palindromo(int n){
+    int aux,resto,numInv=0;
+    aux=n;
+    while(aux>0){
+      resto=aux%10;
+      aux=aux/10;
+      numInv=numInv*10+resto;
+    }
+    if(numInv==n){
+        return true;
+    }
+    else
+       return false;
+}
+int mayorpalin(){
+    int mayor=0;
+    for(int i=100;i<102;i++){
+        for(int j=100;j<102;j++){
+           mayor=i*j;
+           cout<<i<<"x"<<j<<"="<<mayor<<endl;
+        }
+    }
+return 0;
+}
 
 
 //problem 5
